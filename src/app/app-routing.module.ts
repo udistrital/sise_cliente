294f320bd914eaa42ccb0537ff14a423ec9b938f
Duan <duan@simian.co>
2021-03-10 08:26:18 -0500

    Add map page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
@@ -14,23 +14,27 @@ const routes: Routes = [
 
   {
     path: 'info-personal',
-    loadChildren: () => import('./pages/info-personal/info-personal.module').then( m => m.InfoPersonalPageModule)
+    loadChildren: () => import('./pages/info-personal/info-personal.module').then(m => m.InfoPersonalPageModule)
   },
   {
     path: 'info-academica',
-    loadChildren: () => import('./pages/info-academica/info-academica.module').then( m => m.InfoAcademicaPageModule)
+    loadChildren: () => import('./pages/info-academica/info-academica.module').then(m => m.InfoAcademicaPageModule)
   },
   {
     path: 'info-laboral',
-    loadChildren: () => import('./pages/info-laboral/info-laboral.module').then( m => m.InfoLaboralPageModule)
+    loadChildren: () => import('./pages/info-laboral/info-laboral.module').then(m => m.InfoLaboralPageModule)
   },
   {
     path: 'info-empresarial',
-    loadChildren: () => import('./pages/info-empresarial/info-empresarial.module').then( m => m.InfoEmpresarialPageModule)
+    loadChildren: () => import('./pages/info-empresarial/info-empresarial.module').then(m => m.InfoEmpresarialPageModule)
   },
   {
     path: 'localizacion',
-    loadChildren: () => import('./pages/localizacion/localizacion.module').then( m => m.LocalizacionPageModule)
+    loadChildren: () => import('./pages/localizacion/localizacion.module').then(m => m.LocalizacionPageModule)
+  },
+  {
+    path: 'map',
+    loadChildren: () => import('./pages/map/map.module').then(m => m.MapPageModule)
   },
 ];
 
